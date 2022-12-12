a11618939082f7220f905ce8b2689f3178d5ed06
FelipeB4C <felipeb4c@gmail.com>
2022-12-12 00:35:55 -0300

    Adicionando lista de texto estatico

---
+++ app/src/main/java/br/com/felipe/orgs/MainActivity.kt
@@ -0,0 +1,14 @@
+package br.com.felipe.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
