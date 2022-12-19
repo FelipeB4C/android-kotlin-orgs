3878a3e369afe4bc075ad137f3dcf8dab6ebf5a3
FelipeB4C <felipeb4c@gmail.com>
2022-12-19 17:11:08 -0300

    Criando produto no formulario

--- app/src/main/java/br/com/felipe/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/felipe/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import br.com.felipe.orgs.R
+import br.com.felipe.orgs.model.Produto
+import java.math.BigDecimal
 
 class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
 
@@ -17,7 +19,26 @@ class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario
         botaoSalvar.setOnClickListener {
             val campoNome = findViewById<EditText>(R.id.nome)
             val nome = campoNome.text.toString()
-            Log.i("FormularioProduto", "onCreate: $nome")
+
+            val campoDescricao = findViewById<EditText>(R.id.descricao)
+            val descricao = campoDescricao.text.toString()
+
+            val campoValor = findViewById<EditText>(R.id.valor)
+            val valorEmText = campoValor.text.toString()
+            val valor = if (valorEmText.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(valorEmText)
+            }
+
+            val produto = Produto(
+                nome = nome,
+                descricao = descricao,
+                valor = valor
+            )
+
+            Log.i("FormularioProduto", "onCreate: $produto")
+
         }
     }
 
