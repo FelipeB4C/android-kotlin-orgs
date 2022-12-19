553dc48dafb9b0e3cefff02a647d8a362c76b2aa
FelipeB4C <felipeb4c@gmail.com>
2022-12-19 19:18:15 -0300

    Criando botao para ir para o formulario

--- app/src/main/java/br/com/felipe/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/felipe/orgs/ui/activity/MainActivity.kt
@@ -1,25 +1,19 @@
 package br.com.felipe.orgs.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.felipe.orgs.R
 import br.com.felipe.orgs.model.Produto
 import br.com.felipe.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-/*        val nome = findViewById<TextView>(R.id.nome)
-        nome.text = "Cesta de frutas"
-
-        val descricao = findViewById<TextView>(R.id.descricao)
-        descricao.text = "Laranja, manga e maçã"
-
-        val valor = findViewById<TextView>(R.id.valor)
-        valor.text = "19.90"*/
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
@@ -28,8 +22,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             Produto(nome = "Uva", descricao = "Sem caroço", valor = BigDecimal("7.00")),
         ))
 
-        //recyclerView.layoutManager = LinearLayoutManager(this)
-
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener{
+            val intent = Intent(this, FormularioProdutoActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
